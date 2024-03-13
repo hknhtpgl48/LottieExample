0838cca0ef2477b92c59eac8ed46f8f29dad73f7
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-03-13 09:35:24 +0300

    Initial Commit

---
+++ LottieExample/LottieExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieExampleApp.swift
+//  LottieExample
+//
+//  Created by Hakan Hatipoğlu on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LottieExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
